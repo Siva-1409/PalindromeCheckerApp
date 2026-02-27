f2f78c1a95a273f1ad003c5761164e34d3d7c233
Siva-1409 <kk5266@srmist.edu.in>
2026-02-27 10:51:30 +0530

    UC8: Linked List Based

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,18 +1,45 @@
-import java.util.Deque;
-import java.util.ArrayDeque;
 public class PalindromeCheckerApp {
-    public static void main(String[] args) {
-        String input = "refer";
-        Deque<Character> deque = new ArrayDeque<>();
-        for (char c : input.toCharArray()) {
-            deque.addLast(c);
+    static class Node {
+        char data;
+        Node next;
+        Node(char data) {
+            this.data = data;
         }
+    }
+    public static void main(String[] args) {
+        String input = "level";
+        Node head = null, tail = null;
+        for (char c : input.toCharArray()) {
+            Node newNode = new Node(c);
+            if (head == null) {
+                head = tail = newNode;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+            }
+        }
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node prev = null;
+        while (slow != null) {
+            Node next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+        Node first = head;
+        Node second = prev;
         boolean isPalindrome = true;
-        while (deque.size() > 1) {
-            if (deque.removeFirst() != deque.removeLast()) {
+        while (second != null) {
+            if (first.data != second.data) {
                 isPalindrome = false;
                 break;
             }
+            first = first.next;
+            second = second.next;
         }
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
