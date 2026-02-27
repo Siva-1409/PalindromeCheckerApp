20e2c095e198677a621c1cdfb7a29a00a0aa457e
Siva-1409 <kk5266@srmist.edu.in>
2026-02-27 10:54:14 +0530

    UC9: Recursive

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,47 +1,15 @@
 public class PalindromeCheckerApp {
-    static class Node {
-        char data;
-        Node next;
-        Node(char data) {
-            this.data = data;
-        }
-    }
     public static void main(String[] args) {
-        String input = "level";
-        Node head = null, tail = null;
-        for (char c : input.toCharArray()) {
-            Node newNode = new Node(c);
-            if (head == null) {
-                head = tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-        Node slow = head, fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-        Node prev = null;
-        while (slow != null) {
-            Node next = slow.next;
-            slow.next = prev;
-            prev = slow;
-            slow = next;
-        }
-        Node first = head;
-        Node second = prev;
-        boolean isPalindrome = true;
-        while (second != null) {
-            if (first.data != second.data) {
-                isPalindrome = false;
-                break;
-            }
-            first = first.next;
-            second = second.next;
-        }
+        String input = "madam";
+        boolean result = check(input, 0, input.length() - 1);
         System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+        System.out.println("Is Palindrome? : " + result);
+    }
+    private static boolean check(String s, int start, int end) {
+        if (start >= end)
+            return true;
+        if (s.charAt(start) != s.charAt(end))
+            return false;
+        return check(s, start + 1, end - 1);
     }
 }
