22ab12b2fdfb85e71b3b332cdd43d87e32a11d28
Siva-1409 <kk5266@srmist.edu.in>
2026-02-27 09:43:43 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
+        // UC1
         System.out.println("Welcome to Palindrome Checker Management App");
         System.out.println("Version: 1.0");
+        System.out.println();
+        // UC2
+        String input = "madam";
+        boolean isPalindrome = true;
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
