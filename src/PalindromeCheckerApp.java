13b2512835dc99714f6ff82081924daf9bc9144c
Siva-1409 <kk5266@srmist.edu.in>
2026-02-27 09:10:16 +0530

    Base application setup

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,6 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker Management App");
+        System.out.println("Version: 1.0");
+    }
+}
